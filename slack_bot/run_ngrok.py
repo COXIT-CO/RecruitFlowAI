14c68d795f69649ae59e72087043d3fa782192f2
Iryna Mykytyn <iramykytyn@gmail.com>
2023-08-31 13:01:03 +0300

    Updates ngrock with some debug info.

--- slack_bot/run_ngrok.py
+++ slack_bot/run_ngrok.py
@@ -2,12 +2,18 @@ import os
 import sys
 import json
 import logging
-from pyngrok import ngrok
+from pyngrok import ngrok, conf
+from pyngrok.exception import PyngrokNgrokError
 from slack_sdk import WebClient
 from slack_bot.settings import env_settings
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def log_event_callback(log):
+    logger.debug("Ngrok log: %s", log)
+
+conf.get_default().log_event_callback = log_event_callback
 
 def update_bot_manifest(public_url:str):
     """Updates bot manifest with current ngrok tunnel url"""
@@ -27,10 +33,39 @@ def update_bot_manifest(public_url:str):
     else:
         logger.error("Error updating manifest: %s", response["error"])
 
+def get_port(default_port=8000):
+    # Check if "--port" is in the command line arguments
+    if "--port" in sys.argv:
+        # Find the index of "--port"
+        port_index = sys.argv.index("--port")
+
+        # Check if there is another argument after "--port"
+        if port_index + 1 < len(sys.argv):
+            # Get the argument after "--port"
+            port_argument = sys.argv[port_index + 1]
+
+            # Try to convert the argument to an integer
+            try:
+                return int(port_argument)
+            except ValueError:
+                logging.error("Invalid port number '%s'. Using default port %s.", port_argument, default_port)
+        else:
+            logging.error("'--port' argument provided but no port number specified. Using default port %s."
+                          , default_port)
+    else:
+        logging.info("'--port' argument not provided. Using default port %s.", default_port)
+
+    return default_port
+
 
 def setup_ngrok():
     """Create new ngrok tunnel and updates the manifest file"""
-    port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 8000
-    public_url = ngrok.connect(port).public_url
-    logger.info("Running ngrok tunnel %s -> http://localhost:%s", public_url, port)
-    update_bot_manifest(public_url)
+    port = get_port()
+    try:
+        http_tunnel = ngrok.connect(port)
+        public_url = http_tunnel.public_url
+
+        logger.info("Running ngrok tunnel %s -> http://localhost:%s", public_url, port)
+        update_bot_manifest(public_url)
+    except PyngrokNgrokError as e:
+        logger.error("Ngrok exception: ", exec=e)
